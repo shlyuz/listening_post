e5c9d51ffb8914e9f4b53cedb072770395fb94d6
Jonathan Echavarria <jechavarria@reliaquest.com>
2020-11-04 14:57:49 -0500

    Start of listening post interaction with teamserver

--- lib/listening_post.py
+++ lib/listening_post.py
@@ -9,6 +9,7 @@ class Listener(object):
                      "author": "Und3rf10w"
                      }
         super(Listener, self).__init__()
+        self.status = "INITIALIZING"
 
     def setup_management_socket(self):
         """
@@ -16,12 +17,21 @@ class Listener(object):
         :return:
         """
         try:
+            listener.listening_post.log(f"Starting management socket on {listener.addr}:{listener.port}",
+                                        source=f"{listener.listening_post.info['name']}")
             listener.listening_post.management_socket = networking.create_mangement_socket(listener.addr, listener.port)
         except Exception as e:
             listener.listening_post.log(f"Critical error when starting Listening Post management Socket",
                                         level="critical", source=f"{listener.listening_post.info['name']}")
             exit()
 
+    def start_management_interaction(self):
+        # TODO: Loop here until we get a vaild startup init from the Teamserver
+        try:
+            networking.recv_management_frame(listener.listening_post.management_socket)
+        except Exception as e:
+            listener.listening_post.log(f"Invalid attempt to connect on management interface",
+                                        source=f"{listener.listening_post.info['name']}")
 
     def start_lp(self, *args):
         """
@@ -36,10 +46,22 @@ class Listener(object):
         listener = args[0]
         try:
             # Create our management socket
-            self.setup_management_socket(listener)
+            self.setup_management_socket()
 
+            # TODO: Setup Listener transport(s)
 
+            # TODO: Request Manifests from Implants
 
+            # TODO: Update internal values with manifests from Implants
+
+            # TODO: Prepare Listening Post Manifest
+
+            # TODO: Receive management socket for init from teamserver
+            self.setup_management_socket()
+
+            # TODO: Send listening post manifest to teamserver
+
+            # TODO: Await commands from teamserver and perform main loop
 
         except Exception as e:
             listener.listening_post.log(f"Critical error when starting teamserver api server", level="critical",
--- lib/networking.py
+++ lib/networking.py
@@ -15,3 +15,25 @@ def create_mangement_socket(addr, port):
     return (management_channel['sock'])
 
 
+def send_management_frame(sock, chunk):
+    slen = struct.pack('<I', len(chunk))
+    sock.sendall(slen + chunk)
+
+
+def recv_management_frame(sock):
+    try:
+        chunk = sock.recv(4)
+    except:
+        return("")
+
+    if len(chunk) > 4:
+        return()
+
+    slen = struct.unpack('<I', chunk)[0]
+    chunk = sock.recv(slen)
+    while len(chunk) < slen:
+        chunk = chunk + sock.recv(slen - len(chunk))
+
+
+def kill_management_socket(sock):
+    sock.close()
