dcc2a6da29c23360ee0f868d6634c56ddd173fd3
Jonathan Echavarria <jechavarria@reliaquest.com>
2020-11-09 17:38:35 +0000

    Added additional crypto libs, updated networking with code adapted from TS

--- lib/networking.py
+++ lib/networking.py
@@ -1,37 +1,99 @@
 import socket
 import struct
+import pickle
+import binascii
+import ast
+
+import lib.crypto.rc6
+import lib.crypto.hex_encoding
+import lib.crypto.xor
 
 
-def create_mangement_socket(addr, port):
+def connect_to_listener_socket(addr, port):
     """
-    Creates our management socket to interact with the teamserver
-    :param addr: The address to bind the management socket to
-    :param port: The address to bind the management port to
+    Creates our management socket to interact with the listener
+    :param addr:
+    :param port:
     :return:
     """
-    management_channel = {'sock': socket.create_connection((addr, int(port))), 'state': 1}
-    return (management_channel['sock'])
+    management_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    management_channel.connect(addr, port)
+    return management_channel
 
 
-def send_management_frame(sock, chunk):
-    slen = struct.pack('<I', len(chunk))
-    sock.sendall(slen + chunk)
+def send_management_frame(listener, data):
+    slen = struct.pack('<I', len(data))
+    listener.logging.log(f"Encoded data: {data}", level="debug", source="lib.networking")
+    listener.listener.management_socket.sendall(slen + data)
 
 
-def recv_management_frame(sock):
+def recv_management_frame(listener):
     try:
-        chunk = sock.recv(4)
+        chunk = listener.listener.management_socket.recv(4)
     except:
-        return("")
+        return ("")
 
     if len(chunk) > 4:
-        return()
+        return ()
 
     slen = struct.unpack('<I', chunk)[0]
-    chunk = sock.recv(slen)
+    chunk = listener.listener.management_socket.recv(slen)
     while len(chunk) < slen:
-        chunk = chunk + sock.recv(slen - len(chunk))
+        chunk = chunk + listener.listener.management_socket.recv(slen - len(chunk))
 
 
-def kill_management_socket(sock):
-    sock.close()
+def kill_management_socket(listener):
+    listener.listener.management_socket.close()
+
+
+def cook_transmit_frame(listener, data):
+    """
+
+    :param listener:
+    :param data: encoded/encrypted data ready to transmit
+    :return:
+    """
+    # frame looks like {"id": 1, "frame_data": "somearbitrarydata", "chunks": 1}
+
+    transmit_data = lib.crypto.rc6.encrypt(listener.config.config['crypto']['rc6_key'], data.encode('utf-8'))
+
+    encrypted_frames = []
+    for chunk_index in range(len(transmit_data)):
+        frame_chunk = {"frame_id": chunk_index, "data": transmit_data[chunk_index],
+                       "chunk_len": len(transmit_data)}
+        encrypted_frames.append(frame_chunk)
+
+    hex_frames = binascii.hexlify(pickle.dumps(encrypted_frames))
+    hex_frames = lib.crypto.hex_encoding.encode_hex(hex_frames)
+    transmit_frames = lib.crypto.xor.single_byte_xor(hex_frames, 
+                                                     ast.literal_eval(listener.config.config['crypto']['xor_key']))
+    listener.logging.log(f"Encoded data: {transmit_frames}", level="debug", source="lib.networking")
+    return transmit_frames
+    # send_management_frame(listener.listener_management_socket, transmit_frames)
+
+
+def uncook_transmit_frame(listener, frame):
+    """
+
+    :param listener:
+    :param frame:
+    :return:
+    """
+    # frame = recv_managmeent_frame(listener.listener_management_socket)
+    unxord_frame = lib.crypto.xor.single_byte_xor(frame, 
+                                                  ast.literal_eval(listener.config.config['crypto']['xor_key']))
+    unenc_frame = lib.crypto.hex_encoding.decode_hex(unxord_frame)
+    del unxord_frame
+    unsorted_recv_frame = pickle.loads(binascii.unhexlify(unenc_frame))
+    del unenc_frame
+
+    data_list = []
+    sorted_frames = sorted(unsorted_recv_frame, key=lambda i: i['frame_id'])
+    del unsorted_recv_frame
+    for data_index in range(len(sorted_frames)):
+        data_list.append(sorted_frames[data_index]['data'])
+
+    decrypted_data = lib.crypto.rc6.decrypt(listener.config.config['crypto']['rc6_key'], data_list)
+    listener.logging.log(f"Decrypted data: {decrypted_data}", level="debug", source="lib.networking")
+
+    return decrypted_data
